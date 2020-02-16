47bfc0192a20e11156bc39a0438f025737879bc7
Ryan Tong <56896048+ryyvntong@users.noreply.github.com>
2020-02-16 02:05:01 -0500

    test

--- inputLinkTaker.py
+++ inputLinkTaker.py
@@ -11,13 +11,12 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return send_from_directory(directory="/", filename='tuto2.pdf')
 
 
 @app.route('/file')
 def note():
-    return send_from_directory(directory="/", filename='tuto2.pdf')
-
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading ofe fiction novels.</p>"
 
 
 @app.route('/submit')
